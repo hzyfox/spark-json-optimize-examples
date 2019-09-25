2156123c5af81d2071f68b6d773b6c6f041c5a0b
jinyu <jinyu.zyp@alibaba-inc.com>
2019-09-25 17:54:40 +0800

    modify CacheNobench

--- src/main/scala/com/fox/examples/sql/CacheNobench.scala
+++ src/main/scala/com/fox/examples/sql/CacheNobench.scala
@@ -74,7 +74,9 @@ object CacheNobench {
       spark.sql(s"drop table if exists ${dbName}_${tableName}")
 
       val sst =  new Date().getTime
-      spark.sql(s"select ${selectContentString},uuid1,uuid2,uuid3 from ${dbName}.${tableName}").write.format("hive").option("fileFormat","orc").mode("overwrite").saveAsTable(s"${dbName}.${dbName}_${tableName}")
+      val sql = s"select ${selectContentString},uuid1,uuid2,uuid3 from default.newnobench"
+      println("sql")
+      spark.sql(sql).write.format("hive").option("fileFormat","orc").mode("overwrite").saveAsTable(s"${dbName}.${dbName}_${tableName}")
       val eet = new Date().getTime
       println(s"$tableName $i cache: First execution time = ${(eet-sst)/1000.0}s ")
       i +=1;
