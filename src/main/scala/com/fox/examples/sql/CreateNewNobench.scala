fbf989429e2cd74833c0eb06530b3b366f4583fd
jinyu <jinyu.zyp@alibaba-inc.com>
2019-09-25 16:55:16 +0800

    fix Create NewNobench

--- src/main/scala/com/fox/examples/sql/CreateNewNobench.scala
+++ src/main/scala/com/fox/examples/sql/CreateNewNobench.scala
@@ -9,8 +9,9 @@ import org.apache.spark.sql.{Row, SparkSession}
   */
 
 object CreateNewNobench {
+  case class NewNobench(json:String,uuid1:String,uuid2:String,uuid3:String)
   def main(args: Array[String]): Unit = {
-    //  case class NewNobench(json:String,uuid1:String,uuid2:String,uuid3:String)
+
     val spark = SparkSession
       .builder()
       .config("spark.sql.catalogImplementation", "hive")
@@ -21,10 +22,10 @@ object CreateNewNobench {
 
     val df = spark.sql("select * from nobench limit 20000000").map(row=>{
       val json = row.get(0).toString
-      val uuid1 = UUID.randomUUID()
-      val uuid2 = UUID.randomUUID()
-      val uuid3 = UUID.randomUUID()
-      (json,uuid1,uuid2,uuid3)
+      val uuid1 = UUID.randomUUID().toString
+      val uuid2 = UUID.randomUUID().toString
+      val uuid3 = UUID.randomUUID().toString
+      NewNobench(json,uuid1,uuid2,uuid3)
     }).toDF("json","uuid1","uuid2","uuid3")
       .write.format("hive").mode("overwrite").option("fileFormat", "orc").saveAsTable("newnobench")
   }
